8c1f94b64dd89cdcb8ec66b312e261bf5892d3b7
sjtuzyz <32588848+sjtuzyz@users.noreply.github.com>
2020-06-25 15:49:25 +0800

    Add files via upload

---
+++ run.py
@@ -0,0 +1,215 @@
+import cv2
+from tracker import KCFTracker
+import numpy as np
+import sys
+import os
+from yolo import YOLO
+from time import time
+from collections import deque
+from keras import backend
+from PIL import Image
+from sympy import Symbol, Matrix
+import math
+
+def main(yolo):
+    selectingObject = False#use detect not hand
+    initTracking = True
+    onTracking = False
+    duration = 0.01
+    pts = deque(maxlen=50)
+    pts4 = deque(maxlen=2)
+    bx2 = [0,0,0,0] 
+    tracker = KCFTracker(True, True, True)
+    #KF initialization
+    P = np.diag([3000.0, 3000.0, 3000.0, 3000.0])
+    I = np.eye(4)
+    H = np.matrix([[0.0, 0.0, 1.0, 0.0],
+                   [0.0, 0.0, 0.0, 1.0]])
+    ra = 0.15  # 厂商提供
+    R = np.matrix([[ra, 0.0],
+                   [0.0, ra]])
+    noise_ax = 0.3
+    noise_ay = 0.3
+    cv2.namedWindow('tracking')
+    #cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture('./outputcar1.mp4')
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if (selectingObject):
+            continue
+        elif (initTracking):
+            image = Image.fromarray(frame[...,::-1]) #bgr to rgb
+            box,class_names = yolo.detect_image(image)#use yolo to predict
+            ix0=int(box[0][0])
+            iy=int(box[0][1])
+            w=int(box[0][2])
+            h=int(box[0][3])
+            tracker.init([ix0, iy, w, h], frame)
+            # Change to your own parameters/换成自己的目标参数和相机参数
+            #Lenovo camera
+            ixx0=int((box[0][0])+(box[0][2])/2)
+            iyy0=int((box[0][1])+(box[0][3])/2)
+            '''
+            D0 = 1258.775/h # meter,qianhou
+            x_cen = 320
+            xd0 = ixx0 - x_cen
+            # geo similarity
+            X0 =xd0/h*1.75 # meter,zuoyou
+            '''
+            
+            #GH4 camera
+            
+            D0 = 2*( 355.2/ w)  # meter,qianhou
+            x_cen = 640
+            xd0 = ixx0 - x_cen
+            # geo similarity
+            X0 = D0 / 887.9 * xd0  # meter,zuoyou
+            
+            #xiaomi
+            '''
+            D0 = (1978.9/h)  # meter,qianhou
+            x_cen = 703
+            xd0 = ixx0 - x_cen
+            # geo similarity
+            X0 = xd0*(1.54/h)  # meter,zuoyou
+            '''
+            #humw.append(0)
+            #humw.append(w)
+            #DJI
+            '''
+            D0 = (3006.5/h)  # meter,qianhou
+            x_cen = 1361.8
+            xd0 = ixx0 - x_cen
+            # geo similarity
+            X0 = xd0*(1.75/h)  # meter,zuoyou
+            '''
+            state = np.matrix([[X0, D0, 0.0, 0.0]]).T
+            state_2D = np.matrix([[ixx0, iyy0 , 0.0, 0.0]]).T
+            initTracking = False
+            onTracking = True
+        elif (onTracking):
+            t0 = time()
+            boundingbox = tracker.update(frame)
+            x=boundingbox[0]
+            y=boundingbox[1]
+            w=boundingbox[2]
+            h=boundingbox[3]
+            cx = int(x + w / 2)
+            cy = int(y + h / 2)              
+            boundingbox = list(map(int, boundingbox))
+            x1=boundingbox[0]
+            y1=boundingbox[1]
+            w1=boundingbox[2]
+            h1=boundingbox[3]
+            ix=int((boundingbox[0])+(boundingbox[2])/2)
+            iy=int((boundingbox[1])+(boundingbox[3])/2)
+            ht=boundingbox[3]
+            wt=boundingbox[2]
+            # Change to your own parameters/换成自己的目标参数和相机参数
+            '''
+            D= 1258.775/ht # meter,qianhou
+            x_cen = 320
+            xd = ix - x_cen
+            # geo similarity
+            X = xd/ht*1.75 # meter,zuoyou
+            '''
+            
+            D = 2*( 355.2/ wt)  # meter,qianhou
+            x_cen = 640
+            xd = ix - x_cen
+            # geo similarity
+            X = D / 887.9 * xd  # meter,zuoyou
+            
+            '''
+            D = (1978.9/ht)  # meter,qianhou
+            x_cen = 703
+            xd0 = ix0 - x_cen
+            # geo similarity
+            X = xd0*(1.52/ht)  # meter,zuoyou
+            '''
+            '''
+            D = (3006.5/ht)  # meter,qianhou
+            x_cen = 1361.8
+            xd0 = ix0 - x_cen
+            # geo similarity
+            X = xd0*(1.75/ht)
+            '''
+            #K = D/(D+60)
+            
+            #if ():
+            #else:
+               # bx2[0] = int(x+w1/20)
+               # bx2[1] = int(y-h1/20)
+                #bx2[2] = int(w)
+                #bx2[3] = int(h)
+            td=time()
+            dt = td-t0
+            #print(dt)  # Time step between Filters steps
+            F = np.matrix([[1.0, 0.0, dt, 0.0],
+                           [0.0, 1.0, 0.0, dt],
+                           [0.0, 0.0, 1.0, 0.0],
+                           [0.0, 0.0, 0.0, 1.0]])
+            dt_2 = dt * dt
+            dt_3 = dt_2 * dt
+            dt_4 = dt_3 * dt
+            Q = np.matrix([[0.25 * dt_4 * noise_ax, 0, 0.5 * dt_3 * noise_ax, 0],
+                           [0, 0.25 * dt_4 * noise_ay, 0, 0.25 * dt_3 * noise_ay],
+                           [dt_3 / 2 * noise_ax, 0, dt_2 * noise_ax, 0],
+                           [0, dt_3 / 2 * noise_ay, 0, dt_2 * noise_ay]])
+            dX=X-state[0][0]
+            dD=D-state[1][0]
+            dVx=int(dX/dt)
+            dVd=int(dD/dt)
+            dX2=cx-state_2D[0][0]
+            dD2=cy-state_2D[1][0]
+            dVx2=int(dX2/dt)
+            dVd2=int(dD2/dt)
+            state = F * state  # Project the state ahead
+            state_2D = F * state_2D
+            P = F * P * F.T + Q  # Project the error covariance ahead
+            # Measurement Update (Correction)
+            # ==============================
+            S = H * P * H.T + R
+            K = (P * H.T) * np.linalg.pinv(S)
+            # Update the estimate via z
+            Z = ([[float('%.2f'% dVx)],[float('%.2f'% dVd)]])# 本身应该用速度传感器测量真值的，但是由于没有这个传感器所以只能通过视频中算出来的X，D除去对应的dt计算。
+            Z2 = ([[float('%.2f'% dVx2)],[float('%.2f'% dVd2)]])
+            y = Z - (H * state)
+            y2 = Z2 - (H*state_2D)
+            state = state + (K * y)
+            state_2D = state_2D + (K * y2)
+            # update the error convariance
+            P = (I - (K * H)) * P
+            #draw the picture and give out the info of interested obj
+            
+            cv2.rectangle(frame, (boundingbox[0], boundingbox[1]),
+                          (boundingbox[0] + boundingbox[2], boundingbox[1] + boundingbox[3]), (0, 255, 255), 1)
+            center = (int((boundingbox[0])+(boundingbox[2])/2),int((boundingbox[1])+(boundingbox[3])/2))
+            cv2.circle(frame,(center), 1, (0, 255, 255), 5)
+            pts.appendleft(center)
+            for i in range(1,len(pts)):
+                if pts[i-1]is None or pts[i]is None:
+                    continue
+                cv2.line(frame, pts[i-1], pts[i],(0, 255, 255),2)
+            
+
+            t1=time()
+            duration = 0.8 * duration + 0.2 * (t1 - t0)
+            cv2.putText(frame, 'FPS: ' + str(1 / duration)[:4].strip('.'), (8, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(0, 0, 255), 2)
+            cv2.putText(frame, 'Pedestrian status camera-coor: X,D:' + str(state[0][0])+str(state[1][0]), (8, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(0, 0, 255), 2)
+            cv2.putText(frame, 'Pedestrian status camera-coor: Vx,Vd:' + str(state[2][0])+str(state[3][0]), (8, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(0, 0, 255), 2)
+            #print('2D parameter')
+            #print(str(state_2D[2][0]))
+            #print(str(state_2D[3][0]))
+        cv2.imshow('tracking', frame)
+        c = cv2.waitKey(1) & 0xFF
+        if c == 27 or c == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(YOLO())
---
+++ yolo.py
@@ -0,0 +1,155 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Run a YOLO_v3 style detection model on test images.
+"""
+
+import colorsys
+import os
+import random
+from timeit import time
+from timeit import default_timer as timer  ### to calculate FPS
+import cv2
+import numpy as np
+from keras import backend as K
+from keras.models import load_model
+from PIL import Image, ImageFont, ImageDraw
+
+from yolo3.model import yolo_eval
+from yolo3.utils import letterbox_image
+import argparse
+ap = argparse.ArgumentParser()
+#ap.add_argument("-i", "--input",help="path to input video", default = "./test_video/det_t1_video_00315_test.avi")
+ap.add_argument("-c", "--class",help="name of class", default = "person")
+args = vars(ap.parse_args())
+
+class YOLO(object):
+    def __init__(self):
+        self.model_path = './model_data/yolo.h5'
+        self.anchors_path = 'model_data/yolo_anchors.txt'
+        self.classes_path = 'model_data/coco_classes.txt'
+        #具体参数可实验后进行调整
+        if args["class"] == 'person':
+           self.score = 0.6 #0.8
+           self.iou = 0.6
+           self.model_image_size = (416,416)
+        if args["class"] == 'car':
+           self.score = 0.6
+           self.iou = 0.6
+           self.model_image_size = (416, 416)
+        if args["class"] == 'bicycle' or args["class"] == 'motorcycle':
+           self.score = 0.6
+           self.iou = 0.6
+           self.model_image_size = (416, 416)
+        
+        self.class_names = self._get_class()
+        self.anchors = self._get_anchors()
+        self.sess = K.get_session()
+        #self.model_image_size = (416, 416) # fixed size or (None, None) small targets:(320,320) mid targets:(960,960)
+        self.is_fixed_size = self.model_image_size != (None, None)
+        self.boxes, self.scores, self.classes = self.generate()
+
+    def _get_class(self):
+        classes_path = os.path.expanduser(self.classes_path)
+        with open(classes_path) as f:
+            class_names = f.readlines()
+        class_names = [c.strip() for c in class_names]
+        #print(class_names)
+        return class_names
+
+    def _get_anchors(self):
+        anchors_path = os.path.expanduser(self.anchors_path)
+        with open(anchors_path) as f:
+            anchors = f.readline()
+            anchors = [float(x) for x in anchors.split(',')]
+            anchors = np.array(anchors).reshape(-1, 2)
+        return anchors
+
+    def generate(self):
+        model_path = os.path.expanduser(self.model_path)
+        assert model_path.endswith('.h5'), 'Keras model must be a .h5 file.'
+
+        self.yolo_model = load_model(model_path, compile=False)
+        print('{} model, anchors, and classes loaded.'.format(model_path))
+
+        # Generate colors for drawing bounding boxes.
+        hsv_tuples = [(x / len(self.class_names), 1., 1.)
+                      for x in range(len(self.class_names))]
+        self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+        self.colors = list(
+            map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
+                self.colors))
+        random.seed(10101)  # Fixed seed for consistent colors across runs.
+        random.shuffle(self.colors)  # Shuffle colors to decorrelate adjacent classes.
+        random.seed(None)  # Reset seed to default.
+
+        # Generate output tensor targets for filtered bounding boxes.
+        self.input_image_shape = K.placeholder(shape=(2, ))
+        boxes, scores, classes = yolo_eval(self.yolo_model.output, self.anchors,
+                len(self.class_names), self.input_image_shape,
+                score_threshold=self.score, iou_threshold=self.iou)
+        return boxes, scores, classes
+
+    def detect_image(self, image):
+        if self.is_fixed_size:
+            assert self.model_image_size[0]%32 == 0, 'Multiples of 32 required'
+            assert self.model_image_size[1]%32 == 0, 'Multiples of 32 required'
+            boxed_image = letterbox_image(image, tuple(reversed(self.model_image_size)))
+        else:
+            new_image_size = (image.width - (image.width % 32),
+                              image.height - (image.height % 32))
+            boxed_image = letterbox_image(image, new_image_size)
+        image_data = np.array(boxed_image, dtype='float32')
+
+        #print(image_data.shape)
+        image_data /= 255.
+        image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+
+        out_boxes, out_scores, out_classes = self.sess.run(
+            [self.boxes, self.scores, self.classes],
+            feed_dict={
+                self.yolo_model.input: image_data,
+                self.input_image_shape: [image.size[1], image.size[0]],
+                K.learning_phase(): 0
+            })
+        return_boxs = []
+        return_class_name = []
+        person_counter = 0
+        for i, c in reversed(list(enumerate(out_classes))):
+            predicted_class = self.class_names[c]
+            #print(self.class_names[c])
+            #变成你自己想要检测的类别 Change it to the class that you want to detect
+            if  predicted_class != 'person':
+            #if  predicted_class != 'car':
+              # print(predicted_class)
+               continue
+            
+            #if predicted_class != args["class"]:
+               #print(predicted_class)
+               #continue
+            #'''
+            person_counter += 1
+            #if  predicted_class != 'car':
+                #continue
+            #label = predicted_class
+            box = out_boxes[i]
+            #score = out_scores[i]
+            x = int(box[1])
+            y = int(box[0])
+            w = int(box[3]-box[1])
+            h = int(box[2]-box[0])
+            if x < 0 :
+                w = w + x
+                x = 0
+            if y < 0 :
+                h = h + y
+                y = 0
+            return_boxs.append([x,y,w,h])
+            #print(return_boxs)
+            return_class_name.append([predicted_class])
+        #cv2.putText(image, str(self.class_names[c]),(int(box[0]), int(box[1] -50)),0, 5e-3 * 150, (0,255,0),2)
+        #print("Found person: ",person_counter)
+        return return_boxs,return_class_name
+
+    def close_session(self):
+        self.sess.close()
